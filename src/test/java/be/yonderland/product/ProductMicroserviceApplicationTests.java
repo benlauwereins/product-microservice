9c6e1cae00716e2154fe6e777c3c80ebb2cf4892
bel <bel@belga.be>
2022-06-12 15:40:06 +0200

    Initial commit

---
+++ src/test/java/be/yonderland/product/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.yonderland.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
